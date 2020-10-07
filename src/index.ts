22ee7e1a782a094ee35663ec4784e78b95098859
Anton Kalmanovich <grumd0@gmail.com>
2020-10-07 13:41:38 +0300

    Decrease build size by using slice() instead of "..." and Promise instead of async

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,16 @@ function execute<ItemType, ResultType>(
   options: ExecuteOptions<ItemType, ResultType>
 ): Promise<ResultType[]> {
   const { items, job, concurrentLimit, minJobTime } = options;
-  const remainingItems = [...items];
+  const remainingItems = items.slice();
   const results: ResultType[] = [];
 
-  const attachJobStarter = async (item: ItemType): Promise<void> => {
-    const [result] = await Promise.all([job(item), minJobTime ? delay(minJobTime) : undefined]);
-    results.push(result);
-    if (remainingItems.length) {
-      return attachJobStarter(remainingItems.splice(0, 1)[0]);
-    }
+  const attachJobStarter = (item: ItemType): Promise<void> => {
+    return Promise.all([job(item), minJobTime ? delay(minJobTime) : undefined]).then(([result]) => {
+      results.push(result);
+      if (remainingItems.length) {
+        return attachJobStarter(remainingItems.splice(0, 1)[0]);
+      }
+    });
   };
 
   const jobsBatch = remainingItems.splice(0, concurrentLimit || remainingItems.length);
